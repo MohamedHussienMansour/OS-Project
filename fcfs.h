ca023b6e92a9b2334880ca50b71f623594b44f20
Sama-Ahmed-Mohammed <167349546+Sama-Ahmed-Mohammed@users.noreply.github.com>
2025-04-19 16:36:02 +0200

    Update fcfs.h

--- fcfs.h
+++ fcfs.h
@@ -16,23 +16,22 @@ public:
     int turnaround_time;
 
     Process(int id, int arrival, int burst);
-
-    void calculate_times();
+    void calculate_turn_time();         // added
+    void calculate_waiting_time();      // added
 };
 
 class FCFS_Scheduler {
 private:
-    std::vector<Process> processes;
-    std::queue<Process*> ready_queue;
+    std::vector<Process>& processes;
+    std::queue<Process*> ready;
     Process* current;
     int time;
 
     bool all_finished();
 
 public:
-    FCFS_Scheduler();
-    void add_process(int pid, int arrival_time, int burst_time);
-    void run();
+    FCFS_Scheduler(std::vector<Process>& processes_ref); // changed constructor
+    int get_process();
     void print_report();
 };
 
